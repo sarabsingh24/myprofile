76b91f4188d263d33a4654e4c259b950f92ea164
sarabsingh24 <sarabpreet33@gmail.com>
2021-10-05 07:28:46 +0530

    Updates

--- src/component/common/column/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './style.css'
-
-export default function index({ children, column}) {
-    return <div className={column}>{children}</div>;
-}
--- src/component/pages/react-work/foodMenu/WindowWidth.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-export function WindowWidth() {
-  const IsDesktop = useMediaQuery("(min-width:600px)");
-  return { IsDesktop };
-}
